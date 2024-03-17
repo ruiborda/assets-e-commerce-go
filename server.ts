6678fda762ee9fce1dcc6d61e0e2472c5efce857
Rui Borda <bordarui@gmail.com>
2024-03-17 13:21:09 -0500

    add index.html server

---
+++ server.ts
@@ -0,0 +1,18 @@
+const BASE_PATH = "./dist";
+const server = Bun.serve({
+    port: 8047,
+    async fetch(req) {
+        let reqUrlPath = new URL(req.url).pathname;
+        if (reqUrlPath === "/") {
+            reqUrlPath = "/index.html";
+        }
+        const filePath = BASE_PATH + reqUrlPath;
+        const file = Bun.file(filePath);
+        return new Response(file);
+    },
+    error() {
+        return new Response(null, {status: 404});
+    },
+});
+
+console.log(`Listening on ${server.url}`);
